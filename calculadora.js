62d54e2e7edd12d4c633bc47b0079537b32f92a1
Thamyris <thamyriscabral@hotmail.com>
2022-11-28 18:38:20 -0300

    Criada as tags em JS

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,17 @@
+let name2 = document.getElementById('name');
 
+let age = document.getElementById('age');
+
+let peso = document.getElementById('peso');
+
+let altura = document.getElementById('altura');
+
+let bottumOn = document.getElementById('boxBtnEnviar');
+
+let bottumOff = document.getElementById('boxBtnLimpar');
+
+let imc = document.getElementById('imc');
+
+let aviso = document.getElementById('aviso');
+
+let pessoa = document.getElementsByClassName('pessoa');
